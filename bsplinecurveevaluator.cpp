33b34b6a67f2c6f3ee8b289a95b26111fbe16e12
User <User>
2017-04-27 16:57:37 +0800

    Finish Bspline but without wrapping

--- bsplinecurveevaluator.cpp
+++ bsplinecurveevaluator.cpp
@@ -9,63 +9,60 @@ void BsplineCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 	const bool& bWrap) const
 {
 	int iCtrlPtCount = ptvCtrlPts.size();
-	BezierCurveEvaluator bezier;
-
-	//int left = (iCtrlPtCount - 1) % 3 + 1; // points that can't make bezier curve
 
 	ptvEvaluatedCurvePts.clear(); // redraw, global control
+
+	std::vector<Point> myPts;
+	myPts.assign(ptvCtrlPts.begin(), ptvCtrlPts.end());
+
+	if (bWrap)
+	{
+		Point e1(ptvCtrlPts[0].x + fAniLength, ptvCtrlPts[0].y);
+		Point e2(ptvCtrlPts[1].x + fAniLength, ptvCtrlPts[1].y);
+		myPts.push_back(e1);
+		myPts.push_back(e2);
+	}
+	else
+	{
+		// hack
+		myPts.insert(myPts.begin(), ptvCtrlPts[0]);
+		myPts.push_back(ptvCtrlPts[iCtrlPtCount - 1]);
+		ptvEvaluatedCurvePts.push_back(ptvCtrlPts[0]);
+		ptvEvaluatedCurvePts.push_back(ptvCtrlPts[iCtrlPtCount - 1]);
+	}
+
+	// bspline curve
+	for (int i = 0; i < iCtrlPtCount - 3 + 2; ++i)
+	{
+		displayBspline(myPts[i], myPts[i + 1], myPts[i + 2], myPts[i + 3], ptvEvaluatedCurvePts);
+	}
 
 	float x1 = 0.0;
 	float x2 = fAniLength;
 	float y1, y2;
-	Mat4f base(-1, 3, -3, 1, 3, -6, 3, 0, -3, 3, 0, 0, 1, 0, 0, 0);
-	float interval = 0.001;
 
 	if (bWrap) {
 		if ((ptvCtrlPts[0].x + fAniLength) - ptvCtrlPts[iCtrlPtCount - 1].x > 0.0f) {
-			/*
-			if (left == 3)
+			float max = -1;
+			int maxCount = -1;
+			for (int i = 0; i < ptvEvaluatedCurvePts.size(); i++)
 			{
-				Mat4f points(ptvCtrlPts[iCtrlPtCount - 1 - 2].x, ptvCtrlPts[iCtrlPtCount - 1 - 2].y, 0, 0, ptvCtrlPts[iCtrlPtCount - 1 - 1].x, ptvCtrlPts[iCtrlPtCount - 1 - 1].y, 0, 0, ptvCtrlPts[iCtrlPtCount - 1].x, ptvCtrlPts[iCtrlPtCount - 1].y, 0, 0, ptvCtrlPts[0].x + fAniLength, ptvCtrlPts[0].y, 0, 0);
-				Mat4f baseXpoints = base * points;
-				Mat4f poly, ans;
-				bool last = true;
-				for (float j = 0.0; j < 1.0; j += interval)
+				if (ptvEvaluatedCurvePts[i].x > x2)
+					ptvEvaluatedCurvePts[i].x = ptvEvaluatedCurvePts[i].x - x2;
+				else if (ptvEvaluatedCurvePts[i].x > max)
 				{
-					poly = Mat4f(pow(j, 3), pow(j, 2), j, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-					ans = poly * baseXpoints;
-
-					// special cases (x = 0)
-					if (ptvCtrlPts[0].x <= 0.00001)
-						y2 = ptvCtrlPts[0].y;
-
-					if (ans[0][0] > x2)
-					{
-						ptvEvaluatedCurvePts.push_back(Point(ans[0][0] - x2, ans[0][1]));
-						if (last)
-						{
-							y2 = ans[0][1];
-							last = false;
-						}
-					}
-					else
-						ptvEvaluatedCurvePts.push_back(Point(ans[0][0], ans[0][1]));
+					max = ptvEvaluatedCurvePts[i].x;
+					maxCount = i;
 				}
-				y1 = y2;
 			}
-			else
-			{
-				y1 = (ptvCtrlPts[0].y * (fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x) +
-					ptvCtrlPts[iCtrlPtCount - 1].y * ptvCtrlPts[0].x) /
-					(ptvCtrlPts[0].x + fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x);
-				y2 = y1;
-			}*/
+			y2 = ptvCtrlPts[maxCount].y;
+			y1 = y2;
 		}
 		else
 		{
 			y1 = ptvCtrlPts[0].y;
 			y2 = ptvCtrlPts[iCtrlPtCount - 1].y;
-		}	
+		}
 	}
 	else {
 		// if wrapping is off, make the first and last segments of
@@ -76,20 +73,6 @@ void BsplineCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 
 	ptvEvaluatedCurvePts.push_back(Point(x1, y1));
 	ptvEvaluatedCurvePts.push_back(Point(x2, y2));
-
-	// bspline curve
-	for (int i = 0; i < iCtrlPtCount - 3; ++i)
-	{
-		displayBspline(ptvCtrlPts[i], ptvCtrlPts[i + 1], ptvCtrlPts[i + 2], ptvCtrlPts[i + 3], ptvEvaluatedCurvePts);
-	}
-	/*
-	if (!(bWrap && left == 3))
-	{
-		// point left
-		for (int i = 0; i < left; ++i)
-			ptvEvaluatedCurvePts.push_back(Point(ptvCtrlPts[i + iCtrlPtCount - left].x, ptvCtrlPts[i + iCtrlPtCount - left].y));
-	}
-	*/
 }
 
 void BsplineCurveEvaluator::displayBspline(const Point c1, const Point c2, const Point c3, const Point c4, std::vector<Point>& ptvEvaluatedCurvePts) const

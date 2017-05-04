93f6e2a7f8eceb4d159ad486cc4d721e96fdc125
User <User>
2017-05-04 13:04:25 +0800

    Simplify wrap function for bezier curve

--- beziercurveevaluator.cpp
+++ beziercurveevaluator.cpp
@@ -14,50 +14,65 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 
 	ptvEvaluatedCurvePts.clear(); // redraw, global control
 
+	std::vector<Point> myPts;
+	myPts.assign(ptvCtrlPts.begin(), ptvCtrlPts.end());
+
+	if (bWrap && left == 3)
+	{
+		Point e1(ptvCtrlPts[0].x + fAniLength, ptvCtrlPts[0].y);
+		//Point e2(ptvCtrlPts[1].x + fAniLength, ptvCtrlPts[1].y);
+		myPts.push_back(e1);
+		//myPts.push_back(e2);
+		/*
+		Point e3;
+		if (iCtrlPtCount > 2)
+		{
+			e3.x = ptvCtrlPts[2].x + fAniLength;
+			e3.y = ptvCtrlPts[2].y;
+			myPts.push_back(e3);
+		}
+		else
+		{
+			e3.x = ptvCtrlPts[1].x - fAniLength;
+			e3.y = ptvCtrlPts[1].y;
+			myPts.insert(myPts.begin(), e3);
+		}*/
+	}
+
+	// bezier curve
+	for (int i = 0; i < (myPts.size() - 1) / 3; ++i)
+	{
+		displayBezier(myPts[i * 3 + 0], myPts[i * 3 + 1], myPts[i * 3 + 2], myPts[i * 3 + 3], ptvEvaluatedCurvePts);
+	}
+
+	
+	if (!(bWrap && left == 3))
+	{
+		// point left
+		for (int i = 0; i < left; ++i)
+			ptvEvaluatedCurvePts.push_back(Point(ptvCtrlPts[i + iCtrlPtCount - left].x, ptvCtrlPts[i + iCtrlPtCount - left].y));
+	}
+	
 	float x1 = 0.0;
 	float x2 = fAniLength;
 	float y1, y2;
-	Mat4f base(-1, 3, -3, 1, 3, -6, 3, 0, -3, 3, 0, 0, 1, 0, 0, 0);
-	float interval = 0.001;
 
 	if (bWrap) {
 		if ((ptvCtrlPts[0].x + fAniLength) - ptvCtrlPts[iCtrlPtCount - 1].x > 0.0f) {
-			if (left == 3)
+			float max = -1;
+			int maxCount = -1;
+			for (int i = 0; i < ptvEvaluatedCurvePts.size(); i++)
 			{
-				Mat4f points(ptvCtrlPts[iCtrlPtCount - 1 - 2].x, ptvCtrlPts[iCtrlPtCount - 1 - 2].y, 0, 0, ptvCtrlPts[iCtrlPtCount - 1 - 1].x, ptvCtrlPts[iCtrlPtCount - 1 - 1].y, 0, 0, ptvCtrlPts[iCtrlPtCount - 1].x, ptvCtrlPts[iCtrlPtCount - 1].y, 0, 0, ptvCtrlPts[0].x + fAniLength, ptvCtrlPts[0].y, 0, 0);
-				Mat4f baseXpoints = base * points;
-				Mat4f poly, ans;
-				bool last = true;
-				for (float j = 0.0; j < 1.0; j += interval)
+				if (ptvEvaluatedCurvePts[i].x > x2)
+					ptvEvaluatedCurvePts[i].x = ptvEvaluatedCurvePts[i].x - x2;
+				else if (ptvEvaluatedCurvePts[i].x > max)
 				{
-					poly = Mat4f(pow(j, 3), pow(j, 2), j, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0);
-					ans = poly * baseXpoints;
-
-					// special cases (x = 0)
-					if (ptvCtrlPts[0].x <= 0.00001)
-						y2 = ptvCtrlPts[0].y;
-
-					if (ans[0][0] > x2)
-					{
-						ptvEvaluatedCurvePts.push_back(Point(ans[0][0] - x2, ans[0][1]));
-						if (last)
-						{
-							y2 = ans[0][1];
-							last = false;
-						}
-					}
-					else
-						ptvEvaluatedCurvePts.push_back(Point(ans[0][0], ans[0][1]));
-				}	
-				y1 = y2;
+					max = ptvEvaluatedCurvePts[i].x;
+					maxCount = i;
+				}
 			}
-			else
-			{
-				y1 = (ptvCtrlPts[0].y * (fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x) +
-					ptvCtrlPts[iCtrlPtCount - 1].y * ptvCtrlPts[0].x) /
-					(ptvCtrlPts[0].x + fAniLength - ptvCtrlPts[iCtrlPtCount - 1].x);
-				y2 = y1;
-			}
+			y2 = ptvEvaluatedCurvePts[maxCount].y;
+			y1 = y2;
 		}
 		else
 		{
@@ -74,19 +89,6 @@ void BezierCurveEvaluator::evaluateCurve(const std::vector<Point>& ptvCtrlPts,
 
 	ptvEvaluatedCurvePts.push_back(Point(x1, y1));
 	ptvEvaluatedCurvePts.push_back(Point(x2, y2));
-
-	// bezier curve
-	for (int i = 0; i < (iCtrlPtCount - 1) / 3; ++i)
-	{
-		displayBezierSmart(ptvCtrlPts[i * 3 + 0], ptvCtrlPts[i * 3 + 1], ptvCtrlPts[i * 3 + 2], ptvCtrlPts[i * 3 + 3], ptvEvaluatedCurvePts);
-	}
-
-	if (!(bWrap && left == 3))
-	{
-		// point left
-		for (int i = 0; i < left; ++i)
-			ptvEvaluatedCurvePts.push_back(Point(ptvCtrlPts[i + iCtrlPtCount - left].x, ptvCtrlPts[i + iCtrlPtCount - left].y));
-	}
 }
 
 void BezierCurveEvaluator::displayBezier(const Point c1, const Point c2, const Point c3, const Point c4, std::vector<Point>& ptvEvaluatedCurvePts) const
